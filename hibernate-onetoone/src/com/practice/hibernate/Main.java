ef108cd083da5d99ff491d4fb4a8f7bdb01d3827
Inomag <pratiksr12345@gmail.com>
2021-08-15 21:28:04 +0530

    Main

--- hibernate-onetoone/src/com/practice/hibernate/Main.java
+++ hibernate-onetoone/src/com/practice/hibernate/Main.java
@@ -11,19 +11,41 @@ public class Main {
 	public static void main(String[] args) {
 		
 //		// CREATE SESSION FACTORY
-//		SessionFactory factory = new Configuration()
-//				.configure("hibernate.cfg.xml")
-//				.addAnnotatedClass(Student.class)
-//				.buildSessionFactory();
+		SessionFactory factory = new Configuration()
+				.configure("hibernate.cfg.xml")
+				.addAnnotatedClass(Instructor.class)
+				.addAnnotatedClass(InstructorDetail.class)
+				.buildSessionFactory();
 ////		
 //		// CREATE SESSION
-//		Session session = factory.getCurrentSession();
+		Session session = factory.getCurrentSession();
 //		
-//		try {
-//			
-//		}finally {
-//			factory.close();
-//		}
+		try {
+//			Instructor ins = 
+//					new Instructor("Pratik","Gupta","pratiksr12345@gmail.com");
+//			InstructorDetail detail = 
+//					new InstructorDetail("https://github.com/inomag","Coding");
+			
+			Instructor ins = 
+					new Instructor("Priyakshi","Bharali","priyakshi2000@gmail.com");
+			InstructorDetail detail = 
+					new InstructorDetail("https://github.com/priyu","Singing");
+			
+			ins.setInstructorDetail(detail);
+			
+			session.beginTransaction();
+			
+			// SAVE INSTRUCTOR
+			
+			// AS CASCADE TYPE IS ALL... SO DETAILS ARE ALSO SAVED
+			session.save(ins);
+			
+			session.getTransaction().commit();
+			System.out.println("Saved");
+			
+		}finally {
+			factory.close();
+		}
 	}
 
 }
