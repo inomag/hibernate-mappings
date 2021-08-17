bfbfa50df218f38de50dad1fa59e2819594ee05c
Inomag <pratiksr12345@gmail.com>
2021-08-17 17:22:02 +0530

    Eager, Lazy loading

--- hibernate-onetomany-eagerVSlazy/src/com/practice/hibernate/FetchJoinDemo.java
+++ hibernate-onetomany-eagerVSlazy/src/com/practice/hibernate/FetchJoinDemo.java
@@ -6,7 +6,7 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
-public class CreateInstructor {
+public class FetchJoinDemo {
 
 	public static void main(String[] args) {
 		
